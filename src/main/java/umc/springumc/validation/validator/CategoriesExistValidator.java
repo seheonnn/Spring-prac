bfaf1523815c4e177edc88b63788e41cbfc55850
seheonnn <ho78901@naver.com>
2024-01-06 16:07:10 +0900

    Error Template 수정

--- src/main/java/umc/springumc/validation/validator/CategoriesExistValidator.java
+++ src/main/java/umc/springumc/validation/validator/CategoriesExistValidator.java
@@ -1,40 +1,46 @@
 package umc.springumc.validation.validator;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import umc.springumc.apiPayload.code.status.ErrorStatus;
 import umc.springumc.repository.FoodCategoryRepository;
 import umc.springumc.validation.annotaion.ExistCategories;
-
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
 
-    private final FoodCategoryRepository foodCategoryRepository;
+	private final FoodCategoryRepository foodCategoryRepository;
 
-    @Override
-    public void initialize(ExistCategories constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
-    }
+	@Override
+	public void initialize(ExistCategories constraintAnnotation) {
+		ConstraintValidator.super.initialize(constraintAnnotation);
+	}
 
-    @Override
-    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
-//        boolean isValid = values.stream()
-//                .allMatch(value -> foodCategoryRepository.existsById(value)); // Repository 접근은 Service 에서만 하는 것이 좋음
+	@Override
+	// isValid 에서 false 가 반환되면 ConstraintViolationException 이 반환되는데,
+	// @Valid 가 있어 예외가 바로 전달되지 않고 MethodArgumentNotValidException 발생.
+	// MethodArgumentNotValidException 을 ExceptionAdvice 에서 잡아 처리
+	public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+		//        boolean isValid = values.stream()
+		//                .allMatch(value -> foodCategoryRepository.existsById(value)); // Repository 접근은 Service 에서만 하는 것이 좋음
 
-        boolean isValid = values.stream()
-                .allMatch(value -> foodCategoryRepository.existsById(value));
+		boolean isValid = values.stream()
+			.allMatch(value -> foodCategoryRepository.existsById(value));
 
-        if (!isValid) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.getMessage()).addConstraintViolation();
-        }
+		if (!isValid) {
+			context.disableDefaultConstraintViolation();
+			// FOOD_CATEGORY_NOT_FOUND.getMessage()
+			context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.getMessage())
+				.addConstraintViolation();
+		}
 
-        return isValid;
+		return isValid;
 
-    }
+	}
 }
--- src/main/java/umc/springumc/validation/validator/MemberExistsValidator.java
+++ src/main/java/umc/springumc/validation/validator/MemberExistsValidator.java
@@ -1,38 +1,38 @@
 package umc.springumc.validation.validator;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import umc.springumc.apiPayload.code.status.ErrorStatus;
 import umc.springumc.domain.Member;
-import umc.springumc.repository.MemberRepository;
 import umc.springumc.service.MemberService.MemberQueryService;
-import umc.springumc.validation.annotaion.ExistCategories;
 import umc.springumc.validation.annotaion.ExistMember;
-
-import java.util.Optional;
-
 
 @Component
 @RequiredArgsConstructor
 public class MemberExistsValidator implements ConstraintValidator<ExistMember, Long> {
-    private final MemberQueryService memberQueryService;
+	private final MemberQueryService memberQueryService;
 
-    @Override
-    public void initialize(ExistMember constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
-    }
+	@Override
+	public void initialize(ExistMember constraintAnnotation) {
+		ConstraintValidator.super.initialize(constraintAnnotation);
+	}
 
-    @Override
-    public boolean isValid(Long value, ConstraintValidatorContext context) {
-        Optional<Member> target = memberQueryService.findMember(value);
+	@Override
+	public boolean isValid(Long value, ConstraintValidatorContext context) {
+		Optional<Member> target = memberQueryService.findMember(value);
 
-        if (target.isEmpty()) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(ErrorStatus.MEMBER_NOT_FOUND.toString()).addConstraintViolation();
-            return false;
-        }
-        return true;
-    }
+		if (target.isEmpty()) {
+			context.disableDefaultConstraintViolation();
+			// MEMBER_NOT_FOUND.toString()
+			context.buildConstraintViolationWithTemplate(ErrorStatus.MEMBER_NOT_FOUND.toString())
+				.addConstraintViolation();
+			return false;
+		}
+		return true;
+	}
 }
