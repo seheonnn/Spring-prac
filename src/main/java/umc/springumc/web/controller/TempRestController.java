0da16a1daf747e8eb739ba145ad7a9eb4175f645
seheonnn <ho78901@naver.com>
2024-03-26 19:52:14 +0900

    🔨  test: health check 추가

--- src/main/java/umc/springumc/web/controller/TempRestController.java
+++ src/main/java/umc/springumc/web/controller/TempRestController.java
@@ -1,10 +1,11 @@
 package umc.springumc.web.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 import umc.springumc.apiPayload.ApiResponse;
 import umc.springumc.converter.TempConverter;
 import umc.springumc.service.TempService.TempQueryService;
@@ -15,15 +16,21 @@ import umc.springumc.web.dto.TempResponse;
 @RequiredArgsConstructor
 public class TempRestController {
 
-    private final TempQueryService tempQueryService;
+	private final TempQueryService tempQueryService;
 
-    @GetMapping("/test")
-    public ApiResponse<TempResponse.TempTestDTO> testAPI(){
-        return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
-    }
-    @GetMapping("/exception")
-    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag){
-        tempQueryService.CheckFlag(flag);
-        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
-    }
+	@GetMapping("/test")
+	public ApiResponse<TempResponse.TempTestDTO> testAPI() {
+		return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+	}
+
+	@GetMapping("/exception")
+	public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag) {
+		tempQueryService.CheckFlag(flag);
+		return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
+	}
+
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "I'm healthy!";
+	}
 }
